7a4c2ba400366ff44e316f9e287e434846de73a9
natheraf <ma.eric11214@outlook.com>
2025-10-03 23:31:52 -0400

    feat: simple reader and txt upload btn

--- rclient/src/App.js
+++ rclient/src/App.js
@@ -3,13 +3,14 @@ import Container from "@mui/material/Container";
 import * as React from "react";
 
 import "./App.css";
+import { Reader } from "./features/Reader";
 
 function App() {
   return (
     <Router>
       <Container maxWidth="xl" sx={{ mt: "88px" }}>
         <Routes>
-          <Route path="" element={<h1>Home</h1>} />
+          <Route path="" element={<Reader />} />
           <Route path="*" element={<h1>Wrong path</h1>} />
         </Routes>
       </Container>
---
+++ rclient/src/features/Reader.js
@@ -0,0 +1,36 @@
+import * as React from "react";
+import { Box, Button, Stack } from "@mui/material";
+
+export const Reader = () => {
+  const [text, setText] = React.useState(null);
+  const inputFile = React.useRef(null);
+
+  const handleFileChange = (event) => {
+    if (event.target.files && event.target.files.length > 0) {
+      const file = event.target.files[0];
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          setText(e.target.result);
+        };
+        reader.readAsText(file);
+      }
+    }
+  };
+  const handleEpubImportOnClick = () => {
+    inputFile.current.click();
+  };
+
+  return (
+    <Stack>
+      <input type="file" ref={inputFile} onChange={handleFileChange} hidden />
+      <Button onClick={handleEpubImportOnClick}>Upload</Button>
+      <Box
+        sx={{ whiteSpace: "pre-wrap" }}
+        dangerouslySetInnerHTML={{
+          __html: text ?? "upload a txt",
+        }}
+      />
+    </Stack>
+  );
+};
